c0536288336783f61cfa2dd29d099afedc1bbfda
emusem <mmuston@gmail.com>
2014-06-27 14:12:40 +1000

    Reference and Util setup

--- src/main/java/com/emusem/firstmod/FirstMod.java
+++ src/main/java/com/emusem/firstmod/FirstMod.java
@@ -1,15 +1,21 @@
 package com.emusem.firstmod;
 
+import com.emusem.firstmod.proxy.IProxy;
+import com.emusem.firstmod.reference.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="FirstMod", name="First Mod", version="1.7.2-0.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class FirstMod
 {
-    @Mod.Instance("FirstMod")
+    @Mod.Instance(Reference.MOD_ID)
     public static FirstMod instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
---
+++ src/main/java/com/emusem/firstmod/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.emusem.firstmod.reference;
+
+public class Reference
+{
+    public static final String MOD_ID = "FirstMod";
+    public static final String MOD_NAME = "First Mod";
+    public static final String VERSION = "1.7.2-0.0";
+    public static final String CLIENT_PROXY_CLASS = "com.emusem.firstmod.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.emusem.firstmod.proxy.ServerProxy";
+}
